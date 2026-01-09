de0e09740c2aca3f6893eb2333a6726eafd12aa1
Utpal <utpalsen902@gmail.com>
2026-01-09 15:29:23 +0530

    feat(extension): export new StatsProvider and LeaderboardProvider

--- apps/extension/src/views/index.ts
+++ apps/extension/src/views/index.ts
@@ -2,3 +2,6 @@ export { FriendsProvider, type FriendInfo } from './friendsProvider';
 export { FriendRequestsProvider } from './friendRequestsProvider';
 export { ActivityProvider, type ActivityEvent } from './activityProvider';
 export { StatusBarManager } from './statusBarItem';
+export { StatsProvider, type StatsData } from './statsProvider';
+export { LeaderboardProvider } from './leaderboardProvider';
+

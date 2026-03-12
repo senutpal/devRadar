4e28ed31eca8c6054399f6c17931e75cc4463496
Utpal Sen <utpalsen902@gmail.com>
2026-03-13 00:53:02 +0530

    add theme provider to root layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { LayoutShell } from '@/components/layout';
 import { SITE_CONFIG } from '@/lib/constants';
 import { ThemeProvider } from '@/components/theme-provider';
+import { ThemePresetProvider } from '@/lib/theme-context';
 import { AuthProvider } from '@/lib/auth';
 
 const syne = Syne({
@@ -95,17 +96,19 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <AuthProvider>
-              <Toaster position="bottom-right" />
-              <div
-                className="fixed inset-0 pointer-events-none z-0 opacity-[0.015] dark:opacity-[0.015]"
-                style={{
-                  backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)'/%3E%3C/svg%3E")`,
-                }}
-              />
+            <ThemePresetProvider>
+              <AuthProvider>
+                <Toaster position="bottom-right" />
+                <div
+                  className="fixed inset-0 pointer-events-none z-0 opacity-[0.015] dark:opacity-[0.015]"
+                  style={{
+                    backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)'/%3E%3C/svg%3E")`,
+                  }}
+                />
 
-              <LayoutShell>{children}</LayoutShell>
-            </AuthProvider>
+                <LayoutShell>{children}</LayoutShell>
+              </AuthProvider>
+            </ThemePresetProvider>
           </ThemeProvider>
         </div>
       </body>
